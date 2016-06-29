83a79a584db5985c832994a1a6efc39dff4c95db
alizand1992 <ali.zand@sjsu.edu>
2016-06-28 20:28:34 -0700

    DEV-1 make button stylesheet

--- index.php
+++ index.php
@@ -5,3 +5,33 @@
  * Date: 6/28/16
  * Time: 6:51 PM
  */
+
+?>
+
+<html>
+<head>
+    <script
+        src="https://code.jquery.com/jquery-3.0.0.js"
+        integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo="
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/button.css"
+    
+</head>
+<body>
+    <div class="button">
+        Test
+    </div>
+    <div id="top-menu">
+
+    </div>
+    <div id="top-banner">
+
+    </div>
+    <div id="left-banner">
+
+    </div>
+    <div id="main-container">
+
+    </div>
+</body>
+</html>
